d63af31d97d05e0de81250b1025e3c4b3bccf671
Nour <nowraldo@gmail.com>
2024-04-13 19:26:41 +0200

    Fixing files

---
+++ POM Bank System/Classes/LUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System.Classes
+{
+    public class LUser : User, IUser
+    {
+        public void AddNewClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowClientList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateClientInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ POM Bank System/Interfaces/IMidUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System
+{
+    internal interface IMidUser :IUser
+    {
+        void AddNewUser();
+        void DeleteUser();
+        void FindUser();
+    } 
+}
