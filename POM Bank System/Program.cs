3d3d2fe6a757daac9447230fbe79405111ce9adf
Nour <nowraldo@gmail.com>
2024-04-14 18:54:58 +0200

    Meging test1

--- POM Bank System/Program.cs
+++ POM Bank System/Program.cs
@@ -6,17 +6,6 @@ using System.Windows.Forms;
 
 namespace Bank_System
 {
-    internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login_Form());
-        }
-    }
+    
+    
 }
